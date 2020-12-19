7b1f9985ee03afb04522588259827bc2b7c162da
Maxime Hutinet <maxime@hutinet.ch>
2020-12-19 10:06:52 +0100

    Updated website content

--- src/components/About.js
+++ src/components/About.js
@@ -9,11 +9,8 @@ function About(props) {
       <h1>About me.</h1>
           <Row>
             <Col sm={8}> 
-            <p>Howdy! I’m a <span className="highlight">software engineer</span> based in Geneva, Switzerland.</p>
-            <p>
+            <p>Howdy! I’m a <span className="highlight">software engineer</span> based in Geneva, Switzerland.
               I enjoy <span className="highlight">building tools</span> that help people in their everyday life. 
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed <span className="highlight">eiusmod</span> 
-               tempor incididunt ut labore et dolore magna aliqua. 
             </p>
             <p>
             My <span className="highlight">technological stack</span>
